577ec60a3ed8823ccad5bf7eb6b89f1e2746368f
gogadoro0524 <gogadoro0524@gmail.com>
2023-10-22 11:09:43 +0900

    tem

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,10 +1,10 @@
-import firebase from "firebase/app";
+// import firebase from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_ID,
-  storageBucket: process.env.FIREBASE_STORAGE,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE,
+//   messagingSenderId: process.env.FIREBASE_SENDER_ID,
+//   appId: process.env.FIREBASE_ID,
+// };
